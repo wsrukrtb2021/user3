38384c7dd860f8a05d5b4d40ba8a2ec561b7148f
unknown <lorp.lop@yandex.ru>
2021-06-01 16:59:54 +0300

    sdds

---
+++ BolshayaPachka/BolshayaPachka/Classes/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolshayaPachka.Classes
+{
+    class connect
+    {
+        //строка подключения к базе данных
+        public static string String { get; } = "WSR; Initial Catalog = user3; User = user3; password = passuser3;";
+    }
+}
